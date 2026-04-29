7b3428599abbafc8e762f4dd69fec817f1429977
BT_C09_18 <mobileappdev@bths.edu>
2026-04-29 13:47:30 -0400

    Added Color.swift

---
+++ ColorGuessingGame/Color.swift
@@ -0,0 +1,18 @@
+//
+//  Color.swift
+//  ColorGuessingGame
+//
+//  Created by Student on 4/29/26.
+//
+
+import SwiftUI
+
+struct Color: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Color()
+}
