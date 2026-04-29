a9fbb8d8d5b15f0fff58a5c484ffb86c97b2778c
BT_C09_18 <mobileappdev@bths.edu>
2026-04-29 13:35:28 -0400

    Initial Commit

---
+++ ColorGuessingGame/ColorGuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorGuessingGameApp.swift
+//  ColorGuessingGame
+//
+//  Created by Student on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct ColorGuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorGuessingGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorGuessingGame
+//
+//  Created by Student on 4/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
