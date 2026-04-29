6e245466a3f6408342ff18500ac584ae3c679e2a
BT_C09_14 <Hp5680@bths.edu>
2026-04-29 13:57:01 -0400

    Added a datamodel

---
+++ ColorGuessingGame/DataModels.swift
@@ -0,0 +1,16 @@
+//
+//  DataModels.swift
+//  ColorGuessingGame
+//
+//  Created by Student on 4/29/26.
+//
+
+import SwiftUI
+import Foundation
+
+struct ColorAttributes: Codable {
+    var R_Value: Int
+    var G_Value: Int
+    var B_Value: Int
+    var HexCode: String
+}
