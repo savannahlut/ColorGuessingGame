4adab06cfc5261d3728faf7d2264afc84d264fb1
BT_C09_13 <mobileappdev@bths.edu>
2026-05-01 14:00:40 -0400

    API

--- ColorGuessingGame/ApiManager.swift
+++ ColorGuessingGame/ApiManager.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+
+func callAPI(r: Int, g: Int, b: Int) async throws -> ColorResponse?{
+    let urlStr: String = "https://www.thecolorapi.com/id?rgb=\(r),\(g),\(b)&format=json"
+    let url: URL? = URL(string: urlStr)
+    guard let urlUnwrapped = url else {
+        return nil
+    }
+    do {
+        let (data, response) = try await URLSession.shared.data(from: urlUnwrapped)
+        let responseConverted = response as! HTTPURLResponse
+        let colorResponse: ColorResponse = try JSONDecoder().decode(ColorResponse.self, from:data)
+        return colorResponse
+    } catch let error {
+        print(error)
+    }
+    return nil
+}
---
+++ ColorGuessingGame/ColorResponse.swift
@@ -0,0 +1,7 @@
+//
+//  ColorResponse.swift
+//  ColorGuessingGame
+//
+//  Created by Student on 4/30/26.
+//
+
